3929219e81804f7db9788686160982de2f9f0476
Haniel <hanielohn2016@gmail.com>
2021-08-21 22:49:03 -0300

    novas api s

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import pytz
 from datetime import datetime
 from random import randint
 import requests
+import json
 import tweepy as tp
 from keys import *
-
 
 
 # logando na conta api twitter:
@@ -16,9 +16,29 @@ api = tp.API(auth)
 
 print('this is my twitter bot', flush=True)
 
-url = "https://cataas.com/cat"
+# url = "https://cataas.com/cat"
+links = ['https://cataas.com/cat',
+         'https://thatcopy.pw/catapi/rest/', 'https://aws.random.cat/meow']
 
-def tweet_image(url):
+
+def tweet_image():
+    escolhido = randint(0, 2)
+
+    if escolhido == 0:
+        url = 'https://cataas.com/cat'
+        print('Escolhi o 0')
+    if escolhido == 1:
+        res = requests.get(links[1])
+        data = json.loads(res.text)
+        url = data.get("url")
+        print('Escolhi o 1')
+        # data = json.dumps(data)
+    if escolhido == 2:
+        res = requests.get(links[2])
+        data = json.loads(res.text)
+        url = data.get("file")
+        print("Escolhi o 2")
+
     filename = 'temp.jpg'
     request = requests.get(url, stream=True)
     if request.status_code == 200:
@@ -32,12 +52,9 @@ def tweet_image(url):
         print("Unable to download image")
 
 
-# DEV 1427738991890206732
-
-
 while True:
     tz_SP = pytz.timezone('America/Sao_Paulo')
     datetime_SP = datetime.now(tz_SP)
-    tweet_image(url)
+    tweet_image()
     print('publicando imagem... horário: ' + datetime_SP.strftime("%H:%M:%S"))
     time.sleep(randint(600, 3600))
